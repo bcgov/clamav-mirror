404ac4ecbbb73ae3039b020d4215d89d5819a9eb
Jia Hao <lonoise62@gmail.com>
2022-12-15 23:22:57 +0800

    update comment

--- docker/start.py
+++ docker/start.py
@@ -3,16 +3,16 @@
 import os
 import concurrent.futures
 
-# Run a filesystem oldscan every day unless one is in progress.
+# Run an update every hour unless one is in progress.
 os.system("echo \"$(($RANDOM % 60))   *   *   *   *   /entrypoint.sh update > /proc/1/fd/1 2>&1\" > /root.crontab")
 os.system("fcrontab -u root /root.crontab")
 os.system("rm /root.crontab")
 
-# Run cron
+# Run script
 def systemrun(run):
     os.system(run)
 
-
+# Concurrent run on the cron and serve the server
 with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
     runner = ["/usr/sbin/fcron -f", "/entrypoint.sh serve"]
     executor.map(systemrun, runner)
